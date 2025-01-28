a10d83ff4ade5c2cc0bd1f2cfda7af0ae3c92b5f
Gedha Rizka <gedharizka@gmail.com>
2025-01-28 23:36:28 +0700

    ✨ feat(add sandbox project):

---
+++ src/sandbox/CreateDirectory.groovy
@@ -0,0 +1,9 @@
+folder('Sandbox') {
+    displayName('java-maven')
+    description('Pipeline maven java')
+}
+
+// folder('Java/java-maven') {
+//     displayName('Maven')
+//     description('Maven Compiler')
+// }
---
+++ src/sandbox/job_java_maven.groovy
@@ -0,0 +1,101 @@
+import groovy.json.JsonSlurper
+
+def inputFile = readFileFromWorkspace("sandbox.json")
+def InputJSON = new JsonSlurper().parseText(inputFile)
+def project_env = "Sandbox"
+def git_url = "https://github.com/gedharizka/tweet-trend.git"
+
+for (i=0; i<InputJSON.project.size(); i++) {
+
+    def project_name = InputJSON.project[i].repository_name
+    def repository_name = InputJSON.project[i].repository_name
+    def num_to_keep = InputJSON.num_to_keep
+    def repo_owner_name = InputJSON.repo_owner_name
+
+    pipelineJob("${project_env}/${project_name}") {    
+
+        parameters{
+            // need plugin ---List Git Branches Parameter---
+                // remoteURL(git_url)
+        listGitBranches {
+            branchFilter('.*')
+            defaultValue('main')
+            name('BRANCH_OR_TAG')
+            type('PT_BRANCH')
+            remoteURL(git_url)
+            credentialsId('github-credentials')
+            sortMode('DESCENDING')
+            selectedValue('TOP')
+            listSize('100')
+            tagFilter('')
+            quickFilterEnabled(true)
+        }
+        //    gitParameter {
+        //         name('BRANCH_OR_TAG') 
+        //         description('Select a branch or tag to build') 
+        //         type('PT_BRANCH') 
+        //         branch('main') 
+        //         branchFilter('origin/*') 
+        //         tagFilter('*') 
+        //         useRepository('https://github.com/gedharizka/tweet-trend.git') 
+        //         defaultValue('main') 
+        //         selectedValue('TOP') 
+        //         quickFilterEnabled(true) 
+        //         listSize('10') 
+        //         sortMode('ASCENDING_SMART') 
+        //     }
+        }
+
+        logRotator {    
+            numToKeep(num_to_keep)    
+        }        
+
+        definition {
+            cps{
+                sandbox()
+                script('''
+pipeline {
+    agent any
+    environment{
+        PATH = "/opt/apache-maven-3.9.6/bin:$PATH"
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                echo 'Checking out code...'
+                
+            }
+        }
+        stage('Build') {
+            steps {
+                echo " ===> Build started <==="
+                
+            }
+        }
+        stage('Test') {
+            steps {
+                echo " ===> unit test started <==="
+                
+            }
+        }
+
+
+
+    }
+    post {
+        always {
+            echo 'Pipeline complete, cleaning up workspace...'
+            cleanWs()
+        }
+    }
+}
+                ''')
+            }
+            
+        }
+
+
+
+
+    }
+}
